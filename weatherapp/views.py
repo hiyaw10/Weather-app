8a4dc239b7ad7e82a8a69d8784b22027573fc8c8
hiyawamanuel11@gmail.com <114156496+hiyaw10@users.noreply.github.com>
2024-12-04 17:02:05 +0300

    first commit

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,57 +1,41 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import render, HttpResponse
 import requests
 import datetime
 
 
 def home(request):
-   
-    if 'city' in request.POST:
-         city = request.POST['city']
+    if request.method == 'POST' and 'city' in request.POST:
+        city = request.POST['city']
     else:
-         city = 'indore'     
+        city = 'Addis Ababa'
+
+    api_key = "90358d4365aab41c6ebdd9cf13f81808"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
     
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid='
-    PARAMS = {'units':'metric'}
-
-    API_KEY =  ''
-
-    SEARCH_ENGINE_ID = ''
-     
-    query = city + " 1920x1080"
-    page = 1
-    start = (page - 1) * 10 + 1
-    searchType = 'image'
-    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
-
-    data = requests.get(city_url).json()
-    count = 1
-    search_items = data.get("items")
-    image_url = search_items[1]['link']
+    # Define the parameters for the request (e.g., temperature in Celsius)
+    PARAMS = {'units': 'metric'}
     
-
-    try:
-          
-          data = requests.get(url,params=PARAMS).json()
-          description = data['weather'][0]['description']
-          icon = data['weather'][0]['icon']
-          temp = data['main']['temp']
-          day = datetime.date.today()
-
-          return render(request,'weatherapp/index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
+    # Make the API request
+    response = requests.get(url, params=PARAMS)
     
-    except KeyError:
-          exception_occurred = True
-          messages.error(request,'Entered data is not available to API')   
-          # city = 'indore'
-          # data = requests.get(url,params=PARAMS).json()
-          
-          # description = data['weather'][0]['description']
-          # icon = data['weather'][0]['icon']
-          # temp = data['main']['temp']
-          day = datetime.date.today()
+    # If the response status is OK, parse the JSON, else return an error message
+    if response.status_code == 200:
+        data = response.json()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+    else:
+        description = "Error retrieving weather data"
+        icon = "unknown"
+        temp = "N/A"
 
-          return render(request,'weatherapp/index.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'indore' , 'exception_occurred':exception_occurred } )
-               
-    
-    
+    # Get the current date
+    day = datetime.date.today()
+
+    # Render the template with the weather data
+    return render(request, 'index.html', {
+        'description': description,
+        'icon': icon,
+        'temp': temp,
+        'day': day
+    })
